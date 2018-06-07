a6c85935ac4c8894458b6f31faed2b8d07effb61
kansasSamurai <kansasSamurai@users.noreply.github.com>
2018-06-07 17:36:32 -0500

    Begin to add action identifiers

--- src/main/java/org/jwellman/swing/jtable/JTablePropertyAction.java
+++ src/main/java/org/jwellman/swing/jtable/JTablePropertyAction.java
@@ -10,16 +10,36 @@ public class JTablePropertyAction extends AbstractAction {
 	
 	private static final long serialVersionUID = 1L;
 	
+    private int action;
+    
 	private JTable target;
+
+	public static final int ACTION_HIDE_COLUMN = 22;
+	public static final int ACTION_CLEAR_SELECTION = 55;
 	
-    public JTablePropertyAction(String text, JTable table) {
+    public JTablePropertyAction(String text, JTable table, int actionid) {
         super(text);
         this.target = table;
+        this.action = actionid;
     }
  
     @Override
     public void actionPerformed(ActionEvent e) {
-        target.clearSelection();
+        
+        if (target != null) {
+            switch (this.action) {
+            case ACTION_HIDE_COLUMN:
+                break;
+            case ACTION_CLEAR_SELECTION:
+                target.clearSelection();
+                break;
+            default:
+                System.out.println("UNDEFINED Action identifier: " + action);
+            }
+            
+        } else {
+            System.out.println("Action called on NULL target: " + action);
+        }
         
 //        JCheckBox cbLog = (JCheckBox) e.getSource();
 //        if (cbLog.isSelected()) {
