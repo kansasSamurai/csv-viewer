ed3119e26a45ed2dc613df3dbc32036d09872b15
kansasSamurai <kansasSamurai@users.noreply.github.com>
2018-06-08 17:18:30 -0500

    Experiment with BetterJTable and read a CSV file

--- src/main/java/org/jwellman/csvviewer/IssuesBrowser.java
+++ src/main/java/org/jwellman/csvviewer/IssuesBrowser.java
@@ -3,6 +3,7 @@ package org.jwellman.csvviewer;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.GridBagLayout;
@@ -10,10 +11,14 @@ import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.awt.dnd.DropTarget;
+import java.awt.event.ActionEvent;
 import java.io.File;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.AbstractAction;
+import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
@@ -36,8 +41,14 @@ import javax.swing.table.TableColumnModel;
 
 import org.jdesktop.swingx.JXTable;
 import org.jwellman.csvviewer.models.Person;
+import org.jwellman.foundation.swing.XButton;
+import org.jwellman.foundation.swing.XCheckBox;
+import org.jwellman.foundation.swing.XLabel;
+import org.jwellman.foundation.swing.XTextField;
+import org.jwellman.foundation.swing.XToggleButton;
 import org.jwellman.swing.actions.FileActionAware;
 import org.jwellman.swing.component.HorizontalGraphitePanel;
+import org.jwellman.swing.jtable.BetterJTable;
 import org.jwellman.swing.jtable.JTablePropertyAction;
 import org.jwellman.swing.layout.SpringUtilities;
 
@@ -52,13 +63,15 @@ public class IssuesBrowser extends JPanel implements FileActionAware {
 
     private static final long serialVersionUID = 1L;
     
-    private JTable csvTable = new JTable(); // new JXTable(tableModel); // JTable(tableModel);
+    private JTable csvTable = new JXTable(); // new JXTable(tableModel); // JTable(tableModel); // BetterJTable
     
     private static final Border BORDER_EMPTY = BorderFactory.createEmptyBorder(5, 5, 5, 5);
 
     private static final Border BORDER_ETCHED = BorderFactory.createEtchedBorder();
     
     private static final Border BORDER_COMPOUND = BorderFactory.createCompoundBorder(BORDER_EMPTY, BORDER_ETCHED);
+    
+    private static final Font FONT_SEGOE_UI = new Font("Segoe UI", Font.PLAIN, 10);
     
     public IssuesBrowser() {
 
@@ -127,8 +140,22 @@ public class IssuesBrowser extends JPanel implements FileActionAware {
 		return (int) fontSize;
 	}
 
-	private JComponent createCsvTable() {
-		
+    private JComponent createCsvTable(File file) {
+        return createCsvTableV2(file);
+    }
+    
+	private JComponent createCsvTableV1(File file) {
+        final JScrollPane pane = (csvTable instanceof BetterJTable) 
+                ? BetterJTable.createStripedJScrollPane(csvTable) 
+                : new JScrollPane(csvTable);
+        if (csvTable instanceof BetterJTable) {
+            // do nothing (yet?)
+        } else {
+        }
+        pane.setBorder( BORDER_COMPOUND );
+        // pane.setMinimumSize(new Dimension(500,250));
+        // scrollPane.add(csvTable);            
+
 		final EventList<Person> personList = new BasicEventList<>();
 		personList.add(new Person("Anthony Hopkins", 74));
 		personList.add(new Person("Barack Obama", 50));
@@ -179,23 +206,72 @@ public class IssuesBrowser extends JPanel implements FileActionAware {
 
 		csvTable.getTableHeader().setFont(cellRenderer.getFont());
 		//csvTable.getTableHeader().setDefaultRenderer(cellRenderer);
-		csvTable.setColumnSelectionAllowed(true);
+		//csvTable.setColumnSelectionAllowed(true);
 
 		if (csvTable instanceof JTable) {
 	        this.resizeColumnWidth(csvTable);
 		    
 		} else if (csvTable instanceof JXTable) {
-	        ((JXTable)csvTable).packAll(); 
+		    final JXTable t = (JXTable) csvTable;
+	        t.packAll();
+	        t.setFillsViewportHeight(false);
+	        t.setColumnControlVisible(true);
 		    
 		}
 
-		final JScrollPane pane = new JScrollPane(csvTable);
-		pane.setBorder( BORDER_COMPOUND );
-		// pane.setMinimumSize(new Dimension(500,250));
-		// scrollPane.add(csvTable);
 		return pane;
 	}
 
+    private JComponent createCsvTableV2(File file) {
+        final JScrollPane pane = (csvTable instanceof BetterJTable) 
+                ? BetterJTable.createStripedJScrollPane(csvTable) 
+                : new JScrollPane(csvTable);
+        if (csvTable instanceof BetterJTable) {
+            // do nothing (yet?)
+        } else {
+        }
+        pane.setBorder( BORDER_COMPOUND );
+
+        csvTable.setModel(new DelimitedFileTableModel(file, ","));      
+        
+        boolean fillpanel = false;
+        if (fillpanel) {
+            // do nothing; autoresize is on by default
+            // This is generally undesirable as most delimited files will be wider
+            // than the default size of the panel and overflow/scrolling is desirable.
+        } else {
+            csvTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        }
+        
+        final NumberCellRenderer cellRenderer = new NumberCellRenderer("Consolas"); 
+        // order of preference: Consolas , Lucida Console , Bitstream Vera Sans Mono 
+        // Courier New , Lucida Sans Typewriter
+        // following are interesting (* = not monospaced):
+        // OCR A Extended , HP Simplified* , LuzSans-Book* , Rockwell* , Roboto*
+        // Sitka Text* , X-Files*
+        for (int i=0; i<csvTable.getColumnCount(); i++) {
+            final String cname = csvTable.getColumnName(i);
+            csvTable.getColumn(cname).setCellRenderer(cellRenderer);
+        }
+
+        csvTable.getTableHeader().setFont(cellRenderer.getFont());
+        //csvTable.getTableHeader().setDefaultRenderer(cellRenderer);
+        //csvTable.setColumnSelectionAllowed(true);
+
+        if (csvTable instanceof JTable) {
+            this.resizeColumnWidth(csvTable);
+            
+        } else if (csvTable instanceof JXTable) {
+            final JXTable t = (JXTable) csvTable;
+            t.packAll();
+            t.setFillsViewportHeight(false);
+            t.setColumnControlVisible(true);
+            
+        }
+
+        return pane;
+    }
+    
 	private Component createEasternPanel() {
 		JPanel actions = new JPanel();
 		actions.setLayout(new BorderLayout()); //(new BoxLayout(actions, BoxLayout.PAGE_AXIS));
@@ -214,11 +290,16 @@ public class IssuesBrowser extends JPanel implements FileActionAware {
         case 2:
             JPanel boxlayout = new JPanel();
             boxlayout.setLayout(new BoxLayout(boxlayout, BoxLayout.Y_AXIS));
-            boxlayout.add(new JCheckBox(new JTablePropertyAction("Name", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)));
-            boxlayout.add(new JCheckBox(new JTablePropertyAction("Age", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)));
-            boxlayout.add(new JCheckBox(new JTablePropertyAction("Month", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)));
-            boxlayout.add(new JCheckBox(new JTablePropertyAction("Day", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)));
-            boxlayout.add(new JCheckBox(new JTablePropertyAction("Year", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)));
+            boxlayout.add(XCheckBox.create().setFont(FONT_SEGOE_UI).setAction(new JTablePropertyAction("Name", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)).get());
+            boxlayout.add(XCheckBox.create().setFont(FONT_SEGOE_UI).setAction(new JTablePropertyAction("Age", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)).get());
+            boxlayout.add(XCheckBox.create().setFont(FONT_SEGOE_UI).setAction(new JTablePropertyAction("Month", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)).get());
+            boxlayout.add(XCheckBox.create().setFont(FONT_SEGOE_UI).setAction(new JTablePropertyAction("Day", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)).get());
+            boxlayout.add(XCheckBox.create().setFont(FONT_SEGOE_UI).setAction(new JTablePropertyAction("Year", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)).get());
+//            boxlayout.add(new JCheckBox(new JTablePropertyAction("Name", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)));
+//            boxlayout.add(new JCheckBox(new JTablePropertyAction("Age", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)));
+//            boxlayout.add(new JCheckBox(new JTablePropertyAction("Month", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)));
+//            boxlayout.add(new JCheckBox(new JTablePropertyAction("Day", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)));
+//            boxlayout.add(new JCheckBox(new JTablePropertyAction("Year", csvTable, JTablePropertyAction.ACTION_CLEAR_SELECTION)));
 
             JScrollPane pane = new JScrollPane(boxlayout);
             pane.setBorder(null); // This may only be necessary when using Nimbus L&F?
@@ -254,7 +335,7 @@ public class IssuesBrowser extends JPanel implements FileActionAware {
 
 
     private JPanel createDelimiter() {
-        return this.createDelimiterV1();
+        return this.createDelimiterV3();
     }
     
 	private JPanel createDelimiterV1() {
@@ -275,6 +356,77 @@ public class IssuesBrowser extends JPanel implements FileActionAware {
         return p;
     }
 
+    private JPanel createDelimiterV2() {
+        final JPanel p = new JPanel(new SpringLayout());
+        // Before: p.add(new JLabel("Delimiter").setFont(FONT_SEGOE_UI));
+        // After: (next line)
+        p.add(XLabel.create().setText("DELIMITER").setFont(FONT_SEGOE_UI).get());
+        p.add(new JTextField());
+        //p.add(new JLabel(""));
+        
+        final ButtonGroup bg = new ButtonGroup(); 
+        final JPanel buttons = new JPanel(new GridLayout(1,0)); // FlowLayout() by default
+        buttons.add(XButton.create().setText(",").setFont(FONT_SEGOE_UI).addTo(bg).get());
+        buttons.add(XButton.create().setText("~").setFont(FONT_SEGOE_UI).addTo(bg).get());
+        buttons.add(XButton.create().setText("|").setFont(FONT_SEGOE_UI).addTo(bg).get());
+        buttons.add(XButton.create().setText("\\t").setFont(FONT_SEGOE_UI).addTo(bg).get());
+        p.add(buttons);
+        
+        SpringUtilities.makeCompactGrid(p, 1, 3, 1, 1, 1, 1);
+        return p;
+    }
+
+    private JPanel createDelimiterV3() {
+        final JTextField tfield = (JTextField) XTextField.create().setFont(FONT_SEGOE_UI).get();
+        tfield.setEditable(false);
+        
+        final JPanel p = new JPanel(new SpringLayout());
+        p.setBorder(null);
+        // p.add(XLabel.create().setText("DELIMITER").setFont(FONT_SEGOE_UI).get());
+
+        final Dimension d = new Dimension(22, 1);
+        final ButtonGroup bg = new ButtonGroup(); 
+        p.add(HorizontalGraphitePanel.createDefault(Arrays.asList(
+                 HorizontalGraphitePanel.decorateButton((AbstractButton)XToggleButton.create().setAction(new DelimiterSelector(",", tfield)).addTo(bg).get(), null, d)
+                ,HorizontalGraphitePanel.decorateButton((AbstractButton)XToggleButton.create().setAction(new DelimiterSelector("~", tfield)).addTo(bg).get(), null, d)
+                ,HorizontalGraphitePanel.decorateButton((AbstractButton)XToggleButton.create().setAction(new DelimiterSelector("|", tfield)).addTo(bg).get(), null, d)
+                ,HorizontalGraphitePanel.decorateButton((AbstractButton)XToggleButton.create().setAction(new DelimiterSelector("\\t", tfield)).addTo(bg).get(), null, d)
+                ,HorizontalGraphitePanel.decorateButton((AbstractButton)XToggleButton.create().setAction(new DelimiterSelector(">", tfield)).addTo(bg).get(), null, d)
+            )));
+        
+        p.add(tfield);
+        
+        SpringUtilities.makeCompactGrid(p, 1, 2, 0, 0, 1, 1);
+        return p;
+    }
+    
+    @SuppressWarnings("serial")
+    private class DelimiterSelector extends AbstractAction {
+        
+        private JTextField textfield;
+        
+        public DelimiterSelector(String s, JTextField tf) {
+            super(s);
+            this.textfield = tf;
+        }
+        
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() instanceof AbstractButton) {
+                AbstractButton b = (AbstractButton)e.getSource();
+                final String command = b.getActionCommand();
+                if (command.equals(">")) {
+                    this.textfield.setEditable(true);
+                    this.textfield.setText(null);
+                } else {
+                    this.textfield.setEditable(false);
+                    this.textfield.setText(b.getActionCommand());
+                }
+                                
+            }
+        }
+        
+    }
 
     @SuppressWarnings("serial")
 	private class NumberCellRenderer extends DefaultTableCellRenderer  {
@@ -384,9 +536,25 @@ public class IssuesBrowser extends JPanel implements FileActionAware {
     @Override
     public void doSingleFileAction(File file) {
         //jtextarea.read(new FileReader(file),null);
+        
+        
         final BorderLayout layout = (BorderLayout) this.getLayout();
         this.remove(layout.getLayoutComponent(BorderLayout.CENTER));
-        this.add(this.createCsvTable(), BorderLayout.CENTER);
+        this.add(this.createCsvTable(file), BorderLayout.CENTER);
+        
+//        final URL csv = file.toURI().toURL(); // new URL("http://myapp/employees.csv");
+//        DefaultTableModelExt data = new DefaultTableModelExt(url);
+//        TableModelExtTextLoader loader = new TableModelExtTextLoader(",", false, 75);
+//        data.setLoader(loader);
+//        data.startLoading();
+        
+        if (csvTable instanceof JTable) {
+            //
+        } else if (csvTable instanceof JXTable) {
+            final JXTable t = (JXTable) csvTable;
+            t.setColumnControlVisible(false);
+        }
+
         this.validate();
     }
 
