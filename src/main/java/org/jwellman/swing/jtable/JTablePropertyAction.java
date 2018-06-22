74821faadf5c5b8d5761f272b84c0052fd8858ed
kansasSamurai <kansasSamurai@users.noreply.github.com>
2018-06-22 18:20:24 -0500

    Added toggle grid, toggle horizontal/vertical lines

--- src/main/java/org/jwellman/swing/jtable/JTablePropertyAction.java
+++ src/main/java/org/jwellman/swing/jtable/JTablePropertyAction.java
@@ -3,9 +3,16 @@ package org.jwellman.swing.jtable;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
+import javax.swing.AbstractButton;
 import javax.swing.JTable;
 import javax.swing.table.TableColumn;
 
+/**
+ * A class that encapsulates access to all properties of a JTable.
+ * 
+ * @author rwellman
+ *
+ */
 public class JTablePropertyAction extends AbstractAction {
 	
 	private static final long serialVersionUID = 1L;
@@ -18,10 +25,20 @@ public class JTablePropertyAction extends AbstractAction {
 
 	public static final int ACTION_HIDE_COLUMN = 22;
     public static final int ACTION_SHOW_COLUMN = 23;
+    public static final int ACTION_TOGGLE_GRID = 24;
+    public static final int ACTION_TOGGLE_VERTICAL_LINES = 25;
+    public static final int ACTION_TOGGLE_HORIZONTAL_LINES = 26;
 	public static final int ACTION_CLEAR_SELECTION = 55;
 	public static final int ACTION_TOGGLE_AUTORESIZEMODE = 66;
 	public static final int ACTION_TOGGLE_COLUMNSELECTION = 77;
-	
+
+	/**
+	 * 
+	 * @param text
+	 * @param table
+	 * @param actionid
+	 * @param cname originally intended for show/hide column but currently unused in preference of TableColumnManager
+	 */
     public JTablePropertyAction(String text, JTable table, int actionid, String cname) {
         super(text);
         assert(table != null);
@@ -37,8 +54,6 @@ public class JTablePropertyAction extends AbstractAction {
                 break;
             case ACTION_CLEAR_SELECTION:
                 break;
-            default:
-                System.out.println("UNDEFINED Action identifier: " + action);
             }
             
         }
@@ -47,7 +62,10 @@ public class JTablePropertyAction extends AbstractAction {
  
     @Override
     public void actionPerformed(ActionEvent e) {
-        TableColumn column = null;
+        
+        TableColumn column = null; // Reusable TableColumn reference
+        
+        AbstractButton button = null; // A lot of actions/properties can come only from buttons
         
         if (target != null) {
             switch (this.action) {
@@ -65,6 +83,16 @@ public class JTablePropertyAction extends AbstractAction {
             case ACTION_TOGGLE_AUTORESIZEMODE:
                 final boolean isoff = target.getAutoResizeMode() == JTable.AUTO_RESIZE_OFF;
                 target.setAutoResizeMode(isoff ? JTable.AUTO_RESIZE_ALL_COLUMNS : JTable.AUTO_RESIZE_OFF);
+                break;
+            case ACTION_TOGGLE_GRID:
+                button = (AbstractButton) e.getSource();
+                target.setShowGrid(button.isSelected());
+                break;
+            case ACTION_TOGGLE_HORIZONTAL_LINES:
+                target.setShowHorizontalLines(!target.getShowHorizontalLines());
+                break;
+            case ACTION_TOGGLE_VERTICAL_LINES:
+                target.setShowVerticalLines(!target.getShowVerticalLines());
                 break;
             case ACTION_TOGGLE_COLUMNSELECTION:
             	final boolean ison = target.getColumnSelectionAllowed();
