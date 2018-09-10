e68bbe2f75d0b4096d16287c5915b169388c0c8e
kansasSamurai <kansasSamurai@users.noreply.github.com>
2018-09-10 18:26:53 -0500

    No longer extend JPanel and correctly implement the action

--- src/main/java/org/jwellman/csvviewer/DelimiterChooser.java
+++ src/main/java/org/jwellman/csvviewer/DelimiterChooser.java
@@ -7,37 +7,39 @@ import java.util.Arrays;
 import javax.swing.AbstractAction;
 import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
-import javax.swing.JPanel;
-import javax.swing.SpringLayout;
+import javax.swing.JComponent;
 
 import org.jwellman.csvviewer.interfaces.TextChooserAware;
 import org.jwellman.foundation.swing.XToggleButton;
 import org.jwellman.swing.component.HorizontalGraphitePanel;
 
-public class DelimiterChooser extends JPanel implements TextChooserAware {
+public class DelimiterChooser implements TextChooserAware {
 
-	private static final long serialVersionUID = 1L;
+	private String chosenText = ",";
 	
-	private String chosenText;
+	private JComponent userinterface;
 
 	public DelimiterChooser() {
-        this.setLayout(new SpringLayout());
-        this.setBorder(null);
 
         final Dimension d = new Dimension(22, 1);
         final ButtonGroup bg = new ButtonGroup(); 
-        this.add(HorizontalGraphitePanel.createDefault(Arrays.asList(
-                 HorizontalGraphitePanel.decorateButton((AbstractButton)XToggleButton.create().setAction(new DelimiterSelector(",")).addTo(bg).get(), null, d)
+        this.userinterface = HorizontalGraphitePanel.createDefault(Arrays.asList(
+                 HorizontalGraphitePanel.decorateButton((AbstractButton)XToggleButton.create().setAction(new DelimiterSelector(",")).addTo(bg).setSelected(true).get(), null, d)
                 ,HorizontalGraphitePanel.decorateButton((AbstractButton)XToggleButton.create().setAction(new DelimiterSelector("~")).addTo(bg).get(), null, d)
                 ,HorizontalGraphitePanel.decorateButton((AbstractButton)XToggleButton.create().setAction(new DelimiterSelector("|")).addTo(bg).get(), null, d)
                 ,HorizontalGraphitePanel.decorateButton((AbstractButton)XToggleButton.create().setAction(new DelimiterSelector("\\t")).addTo(bg).get(), null, d)
-                ,HorizontalGraphitePanel.decorateButton((AbstractButton)XToggleButton.create().setAction(new DelimiterSelector(">")).addTo(bg).get(), null, d)
-            )));
+                ,HorizontalGraphitePanel.decorateButton((AbstractButton)XToggleButton.create().setAction(new DelimiterSelector("<?>")).addTo(bg).get(), null, d)
+            ));
+        
 	}
 	
 	@Override
 	public String getText() {
 		return this.chosenText;
+	}
+	
+	public JComponent getUI() {
+	    return this.userinterface;
 	}
 
     @SuppressWarnings("serial")
@@ -50,11 +52,15 @@ public class DelimiterChooser extends JPanel implements TextChooserAware {
         @Override
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() instanceof AbstractButton) {
-                AbstractButton b = (AbstractButton)e.getSource();
-                final String command = b.getActionCommand().toLowerCase();
-                if (command.startsWith("choose")) {
-                } else {
-                }                                
+                final AbstractButton b = (AbstractButton)e.getSource();
+                final String command = b.getActionCommand();
+                if (command.equals("<?>")) {
+                    // TODO implement popup/dialog for custom text
+                } else {                    
+                    chosenText = command;
+                    System.out.println("User selected: " + chosenText);
+                }
+                                
             }
         }
         
