b965aed454140eabdab98bc16688f1ac4c3e2091
kansasSamurai <kansasSamurai@users.noreply.github.com>
2018-09-13 17:03:29 -0500

    Integrate new CloseTabPanel

--- src/main/java/org/jwellman/csvviewer/DocumentManager.java
+++ src/main/java/org/jwellman/csvviewer/DocumentManager.java
@@ -5,25 +5,46 @@ import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
+import org.jwellman.swing.jtabbedpane.CloseTabPanel;
+
 /**
+ * Manages creation, view, and deletion of document views (i.e. DataBrowsers)
  * 
- * @author Rick Wellman
+ * @author rwellman
  *
  */
-public class DocumentManager extends JPanel {
+public class DocumentManager extends JPanel implements DataBrowserAware {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private JTabbedPane tabbedPane;
-	
-	public DocumentManager() {
-		super(new BorderLayout());
-		
-		tabbedPane = new JTabbedPane();
-		tabbedPane.addTab("File1", new DataBrowser());
-		tabbedPane.addTab("File2", new DataBrowser());
-		
-		this.add(tabbedPane, BorderLayout.CENTER);
-	}
-	
+    private JTabbedPane tabbedPane;
+    
+    public DocumentManager() {
+        super(new BorderLayout());
+        
+        tabbedPane = new JTabbedPane();
+        
+        tabbedPane.addTab("CSV1", new DataBrowser(this));
+        new CloseTabPanel(tabbedPane, 0);
+        
+        tabbedPane.addTab("CSV2", new DataBrowser(this));
+        new CloseTabPanel(tabbedPane, 1);
+        
+        this.add(tabbedPane, BorderLayout.CENTER);
+    }
+
+    @Override
+    public void updateFilename(String filename) {
+        int idx = this.tabbedPane.getSelectedIndex();
+        
+        Object c = this.tabbedPane.getTabComponentAt(idx);
+        if (c instanceof CloseTabPanel) {
+            CloseTabPanel b = (CloseTabPanel)c;
+            b.setTitle(filename);
+        } else {
+            this.tabbedPane.setTitleAt(idx, filename);            
+        }
+        
+    }
+    
 }
