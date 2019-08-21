7feffc984d2e7917057b2e1c0414145f399afa90
Richard L. Wellman <rcwellman@yahoo.com>
2019-08-21 08:17:47 -0500

    Initialize IconFontSwing and make compatible with Foundation API

--- src/main/java/org/jwellman/csvviewer/CsvViewerApplication.java
+++ src/main/java/org/jwellman/csvviewer/CsvViewerApplication.java
@@ -7,7 +7,7 @@ import jiconfont.swing.IconFontSwing;
 import org.jwellman.foundation.Foundation;
 import org.jwellman.foundation.uContext;
 import org.jwellman.foundation.extend.AbstractSimpleMain;
-import org.jwellman.foundation.interfaces.uiCustomTheme;
+import org.jwellman.foundation.interfaces.uiThemeProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,26 +15,25 @@ import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = WebMvcAutoConfiguration.class)
-public class CsvViewerApplication extends AbstractSimpleMain implements uiCustomTheme {
+public class CsvViewerApplication extends AbstractSimpleMain implements uiThemeProvider {
 
 	public static void main(String[] args) {
 		
 		SpringApplication app = new SpringApplication(CsvViewerApplication.class);
 		app.setWebEnvironment(false);
-		// app.run(args);
-		
-		// TODO move this to global initialization
-		IconFontSwing.register(FontAwesome.getIconFont());
-		IconFontSwing.register(GoogleMaterialDesignIcons.getIconFont());
-		
+
 		new CsvViewerApplication().startup(true, args);
 	}
 
 	private CsvViewerApplication startup(boolean asMainFrame, String[] args) {
 
+		// Global Initialization(s) [ Frameworks, etc. ]
+		IconFontSwing.register(FontAwesome.getIconFont());
+		IconFontSwing.register(GoogleMaterialDesignIcons.getIconFont());
+		
         // Prepare - User Interface Context
         final uContext context = uContext.createContext();
-        context.setTheme(this);
+        context.setThemeProvider(this);
         context.setDimension(85);
 
         // Step 1 - Initialize Swing
@@ -44,10 +43,11 @@ public class CsvViewerApplication extends AbstractSimpleMain implements uiCustom
         mainui = f.registerUI("viewer", new DocumentManager()); // new DataBrowser());
 
         // Step 3 - Use Foundation to create your "window"; give it your UI.
-        window = f.useWindow(mainui);
+        window = f.useWindow(mainui); // f.useWindow(mainui); f.useDesktop(mainui);
         // Step 3a (optional) - Customize your window
-        window.setTitle("CSV Viewer"); 
+        window.setTitle("Data Viewer"); 
         window.setResizable(true);
+        window.setMaximizable(true);
 
 //		final ComponentGlassPane gp = new ComponentGlassPane((JFrame)this.window);		
 //		final DataBrowser b = (DataBrowser)this.mainui.getChild();
@@ -60,13 +60,15 @@ public class CsvViewerApplication extends AbstractSimpleMain implements uiCustom
         // n/a
 
         // Step 5 - Display your User Interface
-        f.showGUI();
+        f.showGUI(window);
 
         return this;
     }
 
 	@Override
-	public void doCustomTheme() {
+	public void doTheme() {
+		// TODO Auto-generated method stub
 		
 	}
+	
 }
