6bc62a192a743d61faa64c012e1abf1c8d7ffb70
kansasSamurai <kansasSamurai@users.noreply.github.com>
2021-09-19 17:35:09 -0500

    minor refactors + comments

--- src/main/java/org/jwellman/csvviewer/CsvViewerApplication.java
+++ src/main/java/org/jwellman/csvviewer/CsvViewerApplication.java
@@ -22,19 +22,54 @@ public class CsvViewerApplication extends AbstractSimpleMain implements uiThemeP
 		SpringApplication app = new SpringApplication(CsvViewerApplication.class);
 		app.setWebEnvironment(false);
 
-		new CsvViewerApplication().startup(true, args);
+		new CsvViewerApplication().startup(false, args);
 	}
 
-	private CsvViewerApplication startup(boolean asMainFrame, String[] args) {
+	/**
+	 * This is the "simple"/simplest version of startup which must always work.
+	 * Particularly in desktop mode, the initial display may not be packed and "nice"
+	 * but that just means it is using defaults and does not mean that there is a logic error.
+	 * This was tested successfully on 12/1/2020 using Foundation snapshot of the same date.
+	 * 
+	 * @param useDesktop
+	 * @param args
+	 * @return
+	 */
+    public CsvViewerApplication startup_simple(boolean useDesktop, String[] args) {
 
-		// Global Initialization(s) [ Frameworks, etc. ]
-		IconFontSwing.register(FontAwesome.getIconFont());
-		IconFontSwing.register(GoogleMaterialDesignIcons.getIconFont());
-		
+        // Global Initialization(s) [ Frameworks, etc. ]
+        IconFontSwing.register(FontAwesome.getIconFont());
+        IconFontSwing.register(GoogleMaterialDesignIcons.getIconFont());
+
+        // Prepare - User Interface Context
+
+        // Step 1 - Initialize Swing
+        final Foundation f = Foundation.init(null); // context
+        f.showGUI( useDesktop 
+                ? f.useDesktop(new DocumentManager()) 
+                : f.useWindow(new DocumentManager()));
+
+        return this;
+    }
+
+    /**
+     * As of 12/1/2020, this version works with Foundation API of the same date.
+     * 
+     * @param useDesktop
+     * @param args
+     * @return
+     */
+    public CsvViewerApplication startup(boolean useDesktop, String[] args) {
+
+        // Global Initialization(s) [ Frameworks, etc. ]
+        IconFontSwing.register(FontAwesome.getIconFont());
+        IconFontSwing.register(GoogleMaterialDesignIcons.getIconFont());
+
         // Prepare - User Interface Context
         final uContext context = uContext.createContext();
         context.setThemeProvider(this);
         context.setDimension(160); //(85);
+        context.setDesktopMode(useDesktop);
 
         // Step 1 - Initialize Swing
         final Foundation f = Foundation.init(context); // context
@@ -43,15 +78,15 @@ public class CsvViewerApplication extends AbstractSimpleMain implements uiThemeP
         mainui = f.registerUI("viewer", new DocumentManager()); // new DataBrowser());
 
         // Step 3 - Use Foundation to create your "window"; give it your UI.
-        window = f.useWindow(mainui); // f.useWindow(mainui); f.useDesktop(mainui);
+        window = useDesktop ? f.useDesktop(mainui) : f.useWindow(mainui);
         // Step 3a (optional) - Customize your window
-        window.setTitle("Data Viewer"); 
+        window.setTitle("Data Viewer");
         window.setResizable(true);
         window.setMaximizable(true);
 
-//		final ComponentGlassPane gp = new ComponentGlassPane((JFrame)this.window);		
-//		final DataBrowser b = (DataBrowser)this.mainui.getChild();
-//		b.getGlassPaneButton().addActionListener(gp);
+//      final ComponentGlassPane gp = new ComponentGlassPane((JFrame)this.window);      
+//      final DataBrowser b = (DataBrowser)this.mainui.getChild();
+//      b.getGlassPaneButton().addActionListener(gp);
 
         // Step 4a - Create data models, controllers, and other non-UI objects
         // n/a
@@ -60,6 +95,7 @@ public class CsvViewerApplication extends AbstractSimpleMain implements uiThemeP
         // n/a
 
         // Step 5 - Display your User Interface
+        if (useDesktop) {} else f.registerDesktopWindow(window); 
         f.showGUI(window);
 
         return this;
--- src/main/java/org/jwellman/swing/jpanel/RestrictedHeightPanel.java
+++ src/main/java/org/jwellman/swing/jpanel/RestrictedHeightPanel.java
@@ -7,10 +7,17 @@ import javax.swing.JPanel;
 
 /**
  * A JPanel extension that simply prevents most* parent layout managers
- * from manipulating its size by returning its preferred size when
+ * from manipulating its height by returning its *preferred* height when
  * its maximum size is requested.
  * 
- * @author Rick
+ * This is most often used when using JPanels to "stack" components
+ * vertically within a vertical BoxLayout which often results in
+ * "extra" space at the bottom of the container.  Without this class,
+ * the layout manager will assign "extra" space in the enclosing JPanel
+ * and allocate it "within" each nested JPanel which does not give 
+ * the desired effect of components "neatly" stacked in a vertical orientation. 
+ * 
+ * @author rwellman
  *
  */
 public class RestrictedHeightPanel extends JPanel {
