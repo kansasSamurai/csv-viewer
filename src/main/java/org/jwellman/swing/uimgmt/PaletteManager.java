a54332df6fd05b1a26553460c91b28d5dc8e4418
kansasSamurai <kansasSamurai@users.noreply.github.com>
2021-09-19 17:32:35 -0500

    Improve look/feature including color "swatches"

--- src/main/java/org/jwellman/swing/uimgmt/PaletteManager.java
+++ src/main/java/org/jwellman/swing/uimgmt/PaletteManager.java
@@ -7,12 +7,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Map;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.jwellman.swing.icon.ColorIcon;
+import org.jwellman.swing.jpanel.RestrictedHeightPanel;
 
 /**
  * 
@@ -22,26 +28,30 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class PaletteManager extends JPanel implements ActionListener {
 
-	// i.e. "red":=0xFF0000, "green":=0x00FF00, "blue":=0x0000FF, "skyblue"=0xNNNNNN
-	private Map<String,Color> paletteMap = new java.util.HashMap<>(); 
-
 	private static PaletteManager singleton = new PaletteManager();
 	
 	public static PaletteManager get() {
 		return singleton;
 	}
 	
-	// may not need this unless I add it to the main gui
-	private JColorChooser chooser = new JColorChooser();
-	
-	private JPanel pnlAllColor;
+	private JPanel pnlPalette;
 
 	private JButton btnNewColor;
+	
+	private JTextField txtNewColor;
 
-	private static final String CMD_NEW = "New Color";
+    // may not need this unless I add it to the main gui
+    private JColorChooser chooser = new JColorChooser();
+    
+    // i.e. "red":=0xFF0000, "green":=0x00FF00, "blue":=0x0000FF, "skyblue"=0xNNNNNN
+    private Map<String,Color> paletteMap = new java.util.HashMap<>(); 
+
+	private static final String CMD_NEW = "Add to Palette";
 	
 	private PaletteManager() {
 		this.setLayout(new BorderLayout());
+		
+		this.txtNewColor = new JTextField("NameTheColor");
 		
 		this.btnNewColor = new JButton(CMD_NEW);
 		this.btnNewColor.addActionListener(this);
@@ -49,11 +59,13 @@ public class PaletteManager extends JPanel implements ActionListener {
 		JPanel p = null; // reusable
 		String[] collection; // reusable
 		
-		p = this.pnlAllColor = new JPanel();
-		p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+		p = this.pnlPalette = new JPanel();
+		p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS));
+		p.setBorder(BorderFactory.createTitledBorder("Palette"));
 		
 		p = new JPanel();
-		p.add(this.btnNewColor);
+		p.add(this.txtNewColor);
+		p.add(this.btnNewColor);		
 		this.add(p, BorderLayout.SOUTH);		
 		
 		String palette = "shadowblue:0F263D midnightblue:173D7B bahamablue:28B3D9 aquablue:4ACEE4 silvergrey:D6DEE8";
@@ -63,14 +75,14 @@ public class PaletteManager extends JPanel implements ActionListener {
 			
 			String strColor = pair[1];
 			if (strColor.startsWith("0x")) ; else strColor = "0x" + strColor;
-			Color color = Color.decode(strColor);
 			
+			final Color color = Color.decode(strColor);			
 			this.paletteMap.put(pair[0], color);
 			
-			this.pnlAllColor.add(p(color, pair[0]));
+			this.pnlPalette.add(p(color, pair[0]));
 		}
 		
-		this.add(this.pnlAllColor , BorderLayout.EAST);
+		this.add(this.pnlPalette , BorderLayout.EAST);
 		this.add(this.chooser, BorderLayout.CENTER);
 	}
 
@@ -80,18 +92,11 @@ public class PaletteManager extends JPanel implements ActionListener {
 		switch (e.getActionCommand()) {
 		case CMD_NEW :
 			final Color newColor = chooser.getColor(); 
-//			JColorChooser.showDialog(
-//                    this, "New Palette Color", Color.black );
-			
-			final Component[] content = pnlAllColor.getComponents();
-				pnlAllColor.removeAll();			
-			for (Component c : content) {
-				pnlAllColor.add(c);
-			}	
-			pnlAllColor.add(p(newColor, "new"));
-			pnlAllColor.add(Box.createVerticalGlue());			
-			pnlAllColor.revalidate(); pnlAllColor.repaint();
-			
+            this.paletteMap.put(this.txtNewColor.getText(), newColor);
+
+			pnlPalette.add(p(newColor, this.txtNewColor.getText()));
+            pnlPalette.revalidate(); // pnlPalette.repaint();
+
 			break;
 		}
 		
@@ -102,10 +107,29 @@ public class PaletteManager extends JPanel implements ActionListener {
 	 * @return
 	 */
 	private JPanel p(Color c, String name) {
-		final JPanel panel = new JPanel(new BorderLayout());
-		panel.setBackground(c);
+		final JPanel panel = new RestrictedHeightPanel (new BorderLayout());
 		
-		panel.add(new JLabel(name), BorderLayout.CENTER); // temp
+        panel.add(new JLabel(name), BorderLayout.WEST); // temp
+
+        int version = 2;
+        switch (version) {
+        case 1: {
+            JPanel color = new JPanel(); 
+            color.setLayout(new BoxLayout(color, BoxLayout.LINE_AXIS));
+            color.setBackground(c);
+            color.add(Box.createHorizontalStrut(20));
+            panel.add(color, BorderLayout.EAST);
+        }
+            break;
+        case 2: {
+            Icon swatch = new ColorIcon(c, 13);
+
+            JPanel color = new JPanel(); // default is FlowLayout 
+            color.add(new JLabel(swatch));
+            panel.add(color, BorderLayout.EAST);
+        }
+            break;
+        }
 		
 		return panel;
 	}
